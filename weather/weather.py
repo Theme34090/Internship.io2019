0931f1b1f56f2be4f6eed7bb2bf7b8a1d2acf67d
Fapbulouz34090 <5batman400@gmail.com>
2019-01-30 22:54:12 +0700

    Add parsing argument from CLI(able to run from CLI)

--- weather/weather.py
+++ weather/weather.py
@@ -1,46 +1,54 @@
 import json
+from argparse import ArgumentParser
+import sys
 
 try:
     import xmltodict
 except ModuleNotFoundError:
     import subprocess
-    import sys
     subprocess.call([sys.executable, "-m", "pip", "install", 'xmltodict'])
     import xmltodict
 
-inputType = int(
-    input('Select input type :\n1 : text\n2 : xml file\n3 : exit program\n'))
+if sys.argv[1]:
+    inputFileName = sys.argv[1]
+    with open(inputFileName, 'r') as read_file:
+        doc = xmltodict.parse(read_file.read())
+else:
+    inputType = int(
+        input('Select input type :\n1 : text\n2 : xml file\n3 : exit program\n'))
 
-if inputType == 1:
-    text = []
-    print('** After finish entering input please press ENTER again **')
-    print('Enter your xml text :')
-    while True:
-        line = str(input())
-        if line:
-            text.append(line)
-        else:
-            text = line.join(text)
-            break
+    if inputType == 1:
+        text = []
+        print('** After finish entering input please press ENTER again **')
+        print('Enter your xml text :')
+        while True:
+            line = str(input())
+            if line:
+                text.append(line)
+            else:
+                text = line.join(text)
+                break
 
-    doc = xmltodict.parse(text)
+        doc = xmltodict.parse(text)
+        inputFileName = str(input('Enter output file name : '))
+        if '.xml' not in inputFileName:
+            inputFileName += '.xml'
 
-    inputFileName = str(input('Enter output file name : '))
+    if inputType == 2:
+        inputFileName = ''
+        tryAgain = True
+        while tryAgain:
+            try:
+                inputFileName = str(input('Enter input file name/location: '))
+                with open(inputFileName, 'r') as read_file:
+                    doc = xmltodict.parse(read_file.read())
+                tryAgain = False
+            except FileNotFoundError:
+                print('File not found. Please try again')
 
-if inputType == 2:
-    inputFileName = ''
-    tryAgain = True
-    while tryAgain:
-        try:
-            inputFileName = str(input('Enter input file name/location: '))
-            with open(inputFileName, 'r') as read_file:
-                doc = xmltodict.parse(read_file.read())
-            tryAgain = False
-        except FileNotFoundError:
-            print('File not found. Please try again')
+    if inputType == 3:
+        exit(0)
 
-if inputType == 3:
-    exit(0)
 with open(inputFileName.replace('.xml', '.json'), 'w') as write_file:
     json.dump(doc, write_file)
 
